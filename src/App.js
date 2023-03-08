b4db4b92952659f7538fb49f75b5a7d210c16bec
Nguyễn Dương Thuý Vy <nguyenduongthuyvy@Nguyens-MacBook-Air.local>
2023-03-08 20:57:37 +0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  let color = count > 0 ? "green" : count < 0 ? "red" : "white";
+
+  const increment = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+  };
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="container">
+      <h1>Counter</h1>
+      <span style={{ color }} id="value">
+        {count}
+      </span>
+
+      <div className="btn-group">
+        <button onClick={decrement} className="btn btn-decrease">
+          DECREASE
+        </button>
+        <button onClick={reset} className="btn btn-reset">
+          RESET
+        </button>
+        <button onClick={increment} className="btn btn-increase">
+          INCREASE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
